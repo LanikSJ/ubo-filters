df45e6a1de1b6cf9ff6d16eb089b4e88bc59aa6f
LanikSJ <laniksj@gmail.com>
2017-03-21 15:44:57 -0700

    Remove git pull from add script

--- add-element.sh
+++ add-element.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git pull origin master
 perl scripts/sorter.pl ubo-filters.txt
 perl scripts/addChecksum.pl ubo-filters.txt
 git add -A; git commit -S -am "$(echo "$1" | sed "s!\.\([^./]*\(/\|$\)\)! \1!")"
