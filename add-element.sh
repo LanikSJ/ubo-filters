bd1145a63bc9529078a296a180b5037c662e7311
LanikSJ <laniksj@gmail.com>
2017-03-21 15:38:44 -0700

    Remove Move.com and add VideoLan

--- add-element.sh
+++ add-element.sh
@@ -3,4 +3,4 @@ git pull origin master
 perl scripts/sorter.pl ubo-filters.txt
 perl scripts/addChecksum.pl ubo-filters.txt
 git add -A; git commit -S -am "$(echo "$1" | sed "s!\.\([^./]*\(/\|$\)\)! \1!")"
-git push origin master
+git push -f origin master
