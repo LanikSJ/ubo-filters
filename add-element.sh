6cb2d027849c3064cf0a5c0c0aa95303e6c2341f
LanikSJ <laniksj@gmail.com>
2017-08-15 18:03:50 -0700

    Execute Scripts in One Line

--- add-element.sh
+++ add-element.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-perl scripts/sorter.pl $1
-perl scripts/addChecksum.pl $1
+perl scripts/sorter.pl $1 && perl scripts/addChecksum.pl $1
 git add -A; git commit -S -am "$(echo "$2" | sed "s!\.\([^./]*\(/\|$\)\)! \1!")"
 git push -f origin master
