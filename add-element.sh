4a9869b03f1a88cdcc7ea354cd0bda4dadd43fb9
Lanik <LanikSJ@users.noreply.github.com>
2018-09-07 20:10:48 -0700

    Move Badge and Fix Errors (#2)

--- add-element.sh
+++ add-element.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 perl scripts/sorter.pl $1 && perl scripts/addChecksum.pl $1
-git add -A; git commit -S -am "$(echo "$2" | sed "s!\.\([^./]*\(/\|$\)\)! \1!")"
+git add -A; git commit -S -am $2
 git push -f origin master
