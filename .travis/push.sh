9938d602550065a88590659499a006b476e7e62a
LanikSJ <laniksj@gmail.com>
2019-02-25 20:36:35 -0800

    Address Codacy Issues

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,29 +1,16 @@
-#!/bin/sh
+#!/bin/#!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-combine_filters() {
-  cd filters
-  files=ls -alh |grep -Ev "drwx|total" |awk '{print $NF}' |grep -i -Ev "combined|readme|ubo"
-  for i in $files; do cat $i |grep -i -Ev "\!|adblock" |sed -r '/^\s*$/d' >> combined-filters.txt ; done
-  cd .. && /checksum-sort.sh filters/combined-filters.txt
-}
+cd filters
+files=ls -alh |grep -Ev "drwx|total" |awk '{print $NF}' |grep -i -Ev "combined|readme|ubo"
+for i in $files; do cat $i |grep -i -Ev "\!|adblock" |sed -r '/^\s*$/d' >> combined-filters.txt ; done
+cd .. && /checksum-sort.sh filters/combined-filters.txt
 
-commit_website_files() {
-  git checkout -b master
-  git add . *.txt
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+git checkout -b master
+git add . *.txt
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/LanikSJ/ubo-filters.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
-}
-
-setup_git
-combine_filters
-commit_website_files
-upload_files
+git remote add origin https://${GH_TOKEN}@github.com/LanikSJ/ubo-filters.git > /dev/null 2>&1
+git push --quiet --set-upstream origin master
