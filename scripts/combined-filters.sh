ca4dea3fcabdd4e62d62e806c2f42988de82bd30
LanikSJ <laniksj@gmail.com>
2022-10-17 11:11:30 -0700

    Update Combined Script to Use More Command

--- scripts/combined-filters.sh
+++ scripts/combined-filters.sh
@@ -5,25 +5,25 @@ cd filters
 cp template.txt combined-filters.txt
 sed -i "s/2020032301/"$date"01/g" combined-filters.txt
 
-< adback-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< adkeeper-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< admaven-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< admeasures-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< bt-contentza-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< cdn-filter-list.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< cryptomining-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< freecounterstat-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< hilltopads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< istripper-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< kitty-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< macupload-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< malware-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< popads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< propellerads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< toradvertising-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< unknown-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< videoadex-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
-< volumedata-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more adback-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more adkeeper-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more admaven-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more admeasures-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more bt-contentza-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more cdn-filter-list.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more cryptomining-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more freecounterstat-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more hilltopads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more istripper-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more kitty-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more macupload-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more malware-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more popads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more propellerads-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more toradvertising-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more unknown-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more videoadex-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
+more volumedata-domains.txt |grep -v ! |sed '/Adblock/d' >> combined-filters.txt
 
 perl ../scripts/addChecksum.pl combined-filters.txt
 perl ../scripts/sorter.pl combined-filters.txt
