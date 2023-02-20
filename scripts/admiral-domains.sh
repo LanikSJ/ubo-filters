5f17120681c6def3b5fee26b088bad365c7bf7c1
Ilan Erenstein <laniksj@gmail.com>
2023-02-20 03:09:47 -0800

    Update Scripts

--- scripts/admiral-domains.sh
+++ scripts/admiral-domains.sh
@@ -5,7 +5,7 @@ olddate=$(cat filters/getadmiral-domains.txt |grep "Last modified" |awk '{print 
 
 cd filters
 ../scripts/remove-lines.sh getadmiral-domains.txt 15 > getadmiral-domains-temp.txt && mv getadmiral-domains-temp.txt getadmiral-domains.txt
-curl -L https://raw.githubusercontent.com/jkrejcha/AdmiraList/master/AdmiraList.txt |sed -e 's/^/||/' -e 's/$/^/' | sort -d >> getadmiral-domains.txt
+curl -L https://raw.githubusercontent.com/jkrejcha/AdmiraList/master/AdmiraList.txt | sed -e 's/^/||/' -e 's/$/^/' | sort -d >> getadmiral-domains.txt
 sed -i "s/$olddate/"$date"01/g" getadmiral-domains.txt
 perl ../scripts/addChecksum.pl getadmiral-domains.txt
 perl ../scripts/sorter.pl getadmiral-domains.txt
