01d771b258ca89fb5a78d0303e4935af38d7fd34
LanikSJ <LanikSJ@users.noreply.github.com>
2023-10-15 21:44:59 -0700

    Update Perl Script Dependency (#442)

--- scripts/admiral-domains.sh
+++ scripts/admiral-domains.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd filters || exit
+cpan install Path:Tiny
 ../scripts/remove-lines.sh getadmiral-domains.txt 15 >getadmiral-domains-temp.txt && mv getadmiral-domains-temp.txt getadmiral-domains.txt
 curl -L https://raw.githubusercontent.com/jkrejcha/AdmiraList/master/AdmiraList.txt | sed -e 's/^/||/' -e 's/$/^/' | sort -d >>getadmiral-domains.txt
 perl ../scripts/addChecksum.pl getadmiral-domains.txt
