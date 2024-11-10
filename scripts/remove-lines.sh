68bbc7fba248ba10cec5cfdac3149c8d31b57c5d
LanikSJ <LanikSJ@users.noreply.github.com>
2024-11-10 01:27:15 -0800

    Update Filters and Scripts (#646)
    * Update Filters and Scripts
    
    * Restyled by shfmt (#647)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- scripts/remove-lines.sh
+++ scripts/remove-lines.sh
@@ -3,10 +3,10 @@
 FILE="$1"
 LINE_NO=$2
 i=1
-while read line; do
-    echo "$line"
-    if [ "$i" -eq "$LINE_NO" ]; then
-        break
-    fi
-    i=$(( i + 1 ))
+while read -r line; do
+  echo "$line"
+  if [ "$i" -eq "$LINE_NO" ]; then
+    break
+  fi
+  i=$((i + 1))
 done <"$FILE"
